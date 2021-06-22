5264fdf105932a326dfceda97b9abd10c5738fcf
Hassan Juma <okothhassanjuma@gmail.com>
2021-06-22 22:44:33 +0300

    compile 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: character to be abs'd
+ *
+ * Return: last digit of n
+ */
+int print_last_digit(int n)
+{
+	n %= 10;
+
+	if (n < 0)
+		n *= -1;
+	_putchar(n + '0');
+	return (n);
+}
