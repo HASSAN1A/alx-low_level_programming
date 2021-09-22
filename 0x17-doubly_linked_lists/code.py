5ab3d7e4cf090ab537be57aa1939459a45cd214f
Hassan Juma <okothhassanjuma@gmail.com>
2021-09-23 01:12:10 +0300

    add api

---
+++ 0x17-doubly_linked_lists/code.py
@@ -0,0 +1,5 @@
+
+Assignment 1:
+Given the following api call: Create a solution using Django, that gives the number and models of vehicles that have been discontinued for the last x years. Discontinued = a model that has not been produced in the last 2 years(2019, 2020). x can either be a single year or a range of years.
+
+API: https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/honda/modelyear/2021?format=json
